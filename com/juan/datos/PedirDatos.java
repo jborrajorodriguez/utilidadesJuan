39dabf24e9e7198c1031792d6639ff694b5d2e5b
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2018-01-26 09:50:15 +0100

    JavaDoc

--- com/juan/datos/PedirDatos.java
+++ com/juan/datos/PedirDatos.java
@@ -6,13 +6,21 @@ import javax.swing.JOptionPane;
  * @author Juan Borrajo Rodriguez Nº5937
  */
 public class PedirDatos {
-    
+    /**
+     * Metodo para leer un mensaje
+     * @param mensaxe tipo string 
+     * @return un String
+     */
     public static String leerString(String mensaxe){
         String dato;
         dato=JOptionPane.showInputDialog(mensaxe);
         return dato;
     }
-    
+    /**
+     * Metodo para leer una variable de tipo int
+     * @param mensaxe
+     * @return devuelve un valor de tipo int
+     */
     public static int leerInt(String mensaxe){
         int dato=Integer.parseInt(JOptionPane.showInputDialog("Dame numero"));
         return dato;
